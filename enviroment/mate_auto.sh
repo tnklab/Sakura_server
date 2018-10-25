a41d6e74ee35ec4349fbaad0a54748a4341865c3
kazu-mottan <kaitoxtennis1997mail@gmail.com>
2018-10-25 15:54:41 +0900

    none

--- enviroment/mate_auto.sh
+++ enviroment/mate_auto.sh
@@ -1,12 +1,31 @@
-PW=$(openssl rsautl -decrypt -inkey ~/.ssh/id_rsa -in password.rsa)
+#====================================================================
+# desktop-enviroment
+#====================================================================
 yes | sudo apt install -y ubuntu-mate-desktop
 yes | sudo apt install -y mate-desktop-environment
 yes | sudo apt install -y mate-desktop-environment-extra
+
+#====================================================================
+# kye-board settiing
+#====================================================================
 yes | sudo apt-get -y install language-pack-ja-base language-pack-ja ibus-mozc
 cd /etc/xrdp
+wget http://www.mail-archive.com/xrdp-devel@lists.sourceforge.net/msg00263/km-e0010411.ini
 yes | sudo chmod 644 km-0411.ini
 yes | sudo ln -s km-0411.ini km-e0010411.ini
 yes | sudo ln -s km-0411.ini km-e0200411.ini
 yes | sudo ln -s km-0411.ini km-e0210411.ini
 yes | sudo /etc/init.d/xrdp restart
+
+#====================================================================
+# remote-setting
+#====================================================================
+yes | sudo apt install -y xrdp
+cat<<EOF>~/.xsession
+export GTX_IM_MODULE=ibus
+export QT_IM_MODULE=ibus
+export XMODIFIERS="@im=ibus"
+ibus-daemon -rdx
+mate-session
+EOF
 yes | sudo reboot
