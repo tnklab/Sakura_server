7708ff08c99cfb9016a197710dac920d27d0ff3f
kazu-mottan <kaitoxtennis1997mail@gmail.com>
2018-10-25 15:46:19 +0900

    pw_none

--- enviroment/mate_auto.sh
+++ enviroment/mate_auto.sh
@@ -1,21 +1,12 @@
-PW = $(openssl rsautl -decrypt -inkey ~/.ssh/id_rsa -in password.rsa)
-sudo su
-expect -c  "
-set timeout 10
-spawn sudo reboot
-expect \"password for ubuntu:\"
-send \"${PW}\n\"
-exit 0
-"
-apt install -y ubuntu-mate-desktop
-apt install -y mate-desktop-environment
-apt install -y mate-desktop-environment-extra
-apt-get -y install language-pack-ja-base language-pack-ja ibus-mozc
+PW=$(openssl rsautl -decrypt -inkey ~/.ssh/id_rsa -in password.rsa)
+yes | sudo apt install -y ubuntu-mate-desktop
+yes | sudo apt install -y mate-desktop-environment
+yes | sudo apt install -y mate-desktop-environment-extra
+yes | sudo apt-get -y install language-pack-ja-base language-pack-ja ibus-mozc
 cd /etc/xrdp
-chmod 644 km-0411.ini
-ln -s km-0411.ini km-e0010411.ini
-ln -s km-0411.ini km-e0200411.ini 
-ln -s km-0411.ini km-e0210411.ini
-/etc/init.d/xrdp restart
-reboot
-
+yes | sudo chmod 644 km-0411.ini
+yes | sudo ln -s km-0411.ini km-e0010411.ini
+yes | sudo ln -s km-0411.ini km-e0200411.ini
+yes | sudo ln -s km-0411.ini km-e0210411.ini
+yes | sudo /etc/init.d/xrdp restart
+yes | sudo reboot
